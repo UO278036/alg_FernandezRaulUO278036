18bf71094067c0eba19f0e746e1477103a9825d1
UO278036 <UO278036@EII-L05-03.aulasuo.uniovi.es>
2022-02-24 16:49:42 +0100

    Sesion 2 clase ordenacion

---
+++ src/main/java/algestudiante/p2/Insercion.java
@@ -0,0 +1,28 @@
+package algestudiante.p2 ;
+
+import algestudiante.p2.Vector;
+
+/** Este programa sirve para ordenar n elementos
+	con un algoritmo de los "malos" (cuadrático)· 
+	Es la INSERCIÓN DIRECTA
+ */
+public class Insercion extends Vector
+{
+	
+	public Insercion(int nElementos) {
+		super(nElementos);
+	}
+
+	/**
+	 * Ordenación por inserción directa
+	 */
+	@Override
+	public void ordenar() {
+		// TODO: Implementación del método de ordenación
+	} 
+
+	@Override
+	public String getNombre() {
+		return "Inserción directa";
+	} 
+} 
