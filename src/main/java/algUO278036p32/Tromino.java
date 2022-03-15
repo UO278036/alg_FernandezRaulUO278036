c6867bafbdaa8769fe127443ef0ac479806cc1e8
Aladino España <Aladino Espaa@DESKTOP-LJRI95S>
2022-03-15 01:32:01 +0100

    Tromino v1

--- src/main/java/algUO278036p32/Tromino.java
+++ src/main/java/algUO278036p32/Tromino.java
@@ -6,7 +6,31 @@ public class Tromino {
 	private static final int CASILLA_HUECO = 0;
 	private static final int CASILLA_VACIA = -1;
 	private int cas_rellena = 1;
-			
+	
+	public static void main (String arg []) 
+    {
+        Tromino t;
+        long t1,t2,cont;
+        int nVeces= Integer.parseInt (arg [0]);
+        boolean b=true;
+
+        for (int n=4;n<=10000;n*=2)
+        {
+            t = new Tromino(n, 1, 1);
+            t1 = System.currentTimeMillis ();
+
+            for (int repeticiones=1; repeticiones<=nVeces;repeticiones++)
+            { 
+                t.tromino(n, 1, 1, 0, 0);
+            } 
+
+            t2 = System.currentTimeMillis ();
+
+            System.out.println (b+" n="+n+ "TIEMPO="+(t2-t1)+"nVeces="+nVeces);
+
+        }
+    }
+	
 	public Tromino(int tam,int x,int y) {
 		//Creamos el tablero del tamaño pasado como parametro
 		tablero = new int[tam][tam];
@@ -23,6 +47,11 @@ public class Tromino {
 		
 	}
 	
+	public void Show() {
+		for(int i=0; i<tablero.)
+		
+	}
+	
 	// Podemos pasar directamente las direcciones del hueco como parametro para 
 	// no tener que buscarlo 
 	public void resuelveTromino(int[][] tablero,int tam, int x, int y, int cordx, int cordy) {
@@ -34,14 +63,14 @@ public class Tromino {
 		@SuppressWarnings("unused")
 		int faltay = 0;
 		// Buscamos el hueco en el tablero
-		for(int i=0; i<tablero.length;i++) {
-			for(int j=0; j<tablero.length;j++) {
-				if(tablero[i][j] == 0) {
-					faltax = i;
-					faltay = j;
-				}
-			}				
-		}
+//		for(int i=0; i<tablero.length;i++) {
+//			for(int j=0; j<tablero.length;j++) {
+//				if(tablero[i][j] == 0) {
+//					faltax = i;
+//					faltay = j;
+//				}
+//			}				
+//		}
 		//------------------------------------------
 		//Resolvemos caso base(suponemos long 2 si no recursivo)
 		if(tam == 2) {
@@ -71,22 +100,34 @@ public class Tromino {
 				tablero[m1+1][m2] = cas_rellena;
 				tablero[m1+1][m2+1] = cas_rellena;
 				cas_rellena = cas_rellena ++;
+											
 			}else if (x <= m1 && x > m2) {
 				cuadrante2x = x;
 				cuadrante2y = y;
+				tablero[m1][m2] = cas_rellena;
+				tablero[m1+1][m2] = cas_rellena;
+				tablero[m1+1][m2+1] = cas_rellena;
+				cas_rellena = cas_rellena ++;
+			}else if (x > m1 && x <= m2) {
+				cuadrante3x = x;
+				cuadrante3y = y;
+				tablero[m1][m2] = cas_rellena;
+				tablero[m1][m2+1] = cas_rellena;
+				tablero[m1+1][m2+1] = cas_rellena;
+				cas_rellena = cas_rellena ++;
+			}else {
+				cuadrante3x = x;
+				cuadrante3y = y;
+				tablero[m1][m2] = cas_rellena;
+				tablero[m1][m2+1] = cas_rellena;
+				tablero[m1+1][m2+1] = cas_rellena;
+				cas_rellena = cas_rellena ++;	
 			}
-			
-			
-		}  
-			
-			
-			
-			
-		
-		
-		
-		
-		
+			resuelveTromino(tablero,mCuadrante,cuadrante1x,cuadrante1y,cordx,cordy);
+			resuelveTromino(tablero,mCuadrante,cuadrante2x,cuadrante2y,cordx,cordy+mCuadrante);
+			resuelveTromino(tablero,mCuadrante,cuadrante3x,cuadrante3y,cordx+mCuadrante,cordy);
+			resuelveTromino(tablero,mCuadrante,cuadrante4x,cuadrante4y,cordx+mCuadrante,cordy+mCuadrante);					
+		}  			
 	}
 		
 }
