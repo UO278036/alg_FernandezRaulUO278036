d77382557eeae9de68eb9c2f2c1cf3eb6d23f13f
Aladino España <Aladino Espaa@DESKTOP-LJRI95S>
2022-03-15 03:09:07 +0100

    tromino vfinal

--- src/main/java/algUO278036p32/Tromino.java
+++ src/main/java/algUO278036p32/Tromino.java
@@ -10,7 +10,7 @@ public class Tromino {
 	public static void main (String arg []) 
     {
         Tromino t;
-        long t1,t2,cont;
+        long t1,t2;
         int nVeces= Integer.parseInt (arg [0]);
         boolean b=true;
 
@@ -21,13 +21,13 @@ public class Tromino {
 
             for (int repeticiones=1; repeticiones<=nVeces;repeticiones++)
             { 
-                t.tromino(n, 1, 1, 0, 0);
+                t.resuelveTromino(n,1,1,0,0);
             } 
 
             t2 = System.currentTimeMillis ();
 
             System.out.println (b+" n="+n+ "TIEMPO="+(t2-t1)+"nVeces="+nVeces);
-
+          
         }
     }
 	
@@ -37,31 +37,33 @@ public class Tromino {
 		//Rellenamos el tablero de casillas vacias -1
 		for(int i=0; i < tam;i++) {	
 			for(int j=0; i<tam;i++) {
-				tablero[i][j] = CASILLA_VACIA;
+				tablero[i][j] = CASILLA_HUECO;
 			}
 		}
 		// Asignamos el hueco al tablero
-		tablero[x][y] = CASILLA_HUECO;
+		tablero[x][y] = CASILLA_VACIA;
 		
 		
 		
 	}
 	
-	public void Show() {
-		for(int i=0; i<tablero.)
-		
+	public  void Show() {
+		for(int i=0; i<tablero.length;i++) {		
+			for(int j=0;j<tablero.length;j++) {		
+				System.out.print(tablero[i][j] +"\t") ;
+			}
+			System.out.println() ;
+		}
+		System.out.println("---------------------------------");
 	}
 	
 	// Podemos pasar directamente las direcciones del hueco como parametro para 
 	// no tener que buscarlo 
-	public void resuelveTromino(int[][] tablero,int tam, int x, int y, int cordx, int cordy) {
+	public void resuelveTromino(int tam, int x, int y, int cordx, int cordy) {
 		int mCuadrante = tam/2;
 		int m1 = cordx + mCuadrante -1;
 		int m2 = cordy + mCuadrante -1;
-		@SuppressWarnings("unused")
-		int faltax = 0;
-		@SuppressWarnings("unused")
-		int faltay = 0;
+		
 		// Buscamos el hueco en el tablero
 //		for(int i=0; i<tablero.length;i++) {
 //			for(int j=0; j<tablero.length;j++) {
@@ -73,27 +75,35 @@ public class Tromino {
 //		}
 		//------------------------------------------
 		//Resolvemos caso base(suponemos long 2 si no recursivo)
-		if(tam == 2) {
-			for(int i=0; i<tablero.length;i++) {
-				for(int j=0; j<tablero.length;j++) {
-					if(tablero[i][j] != CASILLA_HUECO) {
-						tablero [i][j] = CASILLA_VACIA;
-					}
-				}
-			}
+		if(tam <= 2) {
+//			for(int i=x; i<x+2;i++) {
+//				for(int j=y; j<y+2;j++) {
+//					if(tablero[i][j] != CASILLA_VACIA) {
+//						tablero [i][j] = 1;
+//					}
+//				}
+//			}
+			int punto = tablero[x][y];
+			tablero[m1][m2]=1;
+			tablero[m1][m2+1]=1;
+			tablero[m1+1][m2]=1;
+			tablero[m1+1][m2+1]=1;
+			tablero[x][y]=punto;
+			
 		}
+
 		//---------------------------------------------------
 		if(tam >=4) {
 			int cuadrante1x = m1; 	
 			int cuadrante2x = m1;
 			int cuadrante3x = m1+1;
 			int cuadrante4x = m1+1;
-			int cuadrante1y = m2;
-			int cuadrante2y = m2;
-			int cuadrante3y = m2+1;
-			int cuadrante4y = m2+1;
 			
-			if(x < m1 && x <m2) {
+			int cuadrante1y = m2;
+			int cuadrante2y = m2+1;
+			int cuadrante3y = m2;
+			int cuadrante4y = m2+1;		
+			if(x <= m1 && y <=m2) {
 				cuadrante1x = x;
 				cuadrante1y = y;
 				tablero[m1][m2+1] = cas_rellena;
@@ -101,14 +111,14 @@ public class Tromino {
 				tablero[m1+1][m2+1] = cas_rellena;
 				cas_rellena = cas_rellena ++;
 											
-			}else if (x <= m1 && x > m2) {
+			}else if (x <= m1 && y > m2) {
 				cuadrante2x = x;
 				cuadrante2y = y;
 				tablero[m1][m2] = cas_rellena;
 				tablero[m1+1][m2] = cas_rellena;
 				tablero[m1+1][m2+1] = cas_rellena;
 				cas_rellena = cas_rellena ++;
-			}else if (x > m1 && x <= m2) {
+			}else if (x > m1 && y <= m2) {
 				cuadrante3x = x;
 				cuadrante3y = y;
 				tablero[m1][m2] = cas_rellena;
@@ -116,17 +126,18 @@ public class Tromino {
 				tablero[m1+1][m2+1] = cas_rellena;
 				cas_rellena = cas_rellena ++;
 			}else {
-				cuadrante3x = x;
-				cuadrante3y = y;
+				cuadrante4x = x;
+				cuadrante4y = y;
 				tablero[m1][m2] = cas_rellena;
+				tablero[m1+1][m2] = cas_rellena;
 				tablero[m1][m2+1] = cas_rellena;
-				tablero[m1+1][m2+1] = cas_rellena;
 				cas_rellena = cas_rellena ++;	
+				
 			}
-			resuelveTromino(tablero,mCuadrante,cuadrante1x,cuadrante1y,cordx,cordy);
-			resuelveTromino(tablero,mCuadrante,cuadrante2x,cuadrante2y,cordx,cordy+mCuadrante);
-			resuelveTromino(tablero,mCuadrante,cuadrante3x,cuadrante3y,cordx+mCuadrante,cordy);
-			resuelveTromino(tablero,mCuadrante,cuadrante4x,cuadrante4y,cordx+mCuadrante,cordy+mCuadrante);					
+			resuelveTromino(mCuadrante,cuadrante1x,cuadrante1y,cordx,cordy);
+			resuelveTromino(mCuadrante,cuadrante2x,cuadrante2y,cordx,cordy+mCuadrante);
+			resuelveTromino(mCuadrante,cuadrante3x,cuadrante3y,cordx+mCuadrante,cordy);
+			resuelveTromino(mCuadrante,cuadrante4x,cuadrante4y,cordx+mCuadrante,cordy+mCuadrante);			
 		}  			
 	}
 		
