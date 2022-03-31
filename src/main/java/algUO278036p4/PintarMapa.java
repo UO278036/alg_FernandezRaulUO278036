3be0869f7d5b3627ce955648e57c6db08e209467
uo278036 <uo278036@EII-L05-03.mshome.net>
2022-03-31 16:51:21 +0200

    Version inicial de Promediador de imagen

---
+++ src/main/java/algUO278036p4/PintarMapa.java
@@ -0,0 +1,16 @@
+package algUO278036p4;
+
+import java.util.Arrays;
+
+public class PintarMapa{
+    
+    static int matrizAdyacencia[][];
+    static int[] colores;
+    static int nColores;
+    static int nNodos;
+    
+    /* ----------------- IMPLEMENTACIÓN DEL ALGORITMO ----------------- */
+//
+    
+
+}
