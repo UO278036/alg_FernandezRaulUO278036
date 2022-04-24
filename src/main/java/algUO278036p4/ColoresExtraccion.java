b5c5622ea95801f7156055153cc83ef7a74bd305
Aladino España <Aladino Espaa@DESKTOP-LJRI95S>
2022-04-24 23:23:11 +0200

    Terminada practica 4

---
+++ src/main/java/algUO278036p4/ColoresExtraccion.java
@@ -0,0 +1,34 @@
+package algUO278036p4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class ColoresExtraccion {
+	
+public static void loadFile (String nombreFicheroEntrada, List<String> listaColores) {
+		
+	    String linea;
+	    String[] datosCliente= null;	   
+	    
+	    try {
+	    	   BufferedReader fichero = new BufferedReader(new FileReader(nombreFicheroEntrada));
+	    		while (fichero.ready()) {
+	    			linea = fichero.readLine();
+	    			listaColores.add(linea);
+	    		}
+	    		fichero.close();
+	    }
+	    catch (FileNotFoundException fnfe) {
+	      System.out.println("El archivo no se ha encontrado.");
+	    }
+	    catch (IOException ioe) {
+	      new RuntimeException("Error de entrada/salida.");
+	    } 
+	  }
+	
+}
